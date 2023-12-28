a8894b58a46a7d7b3ba45903dbb2e59177f6088b
Salvatore Campisano <1342203+sal-Camp@users.noreply.github.com>
2023-12-28 08:39:23 -0500

    4.1

--- src/Server.cs
+++ src/Server.cs
@@ -12,17 +12,47 @@ var buffer = new byte[1024];
 socket.Receive(buffer);
 
 var request = Encoding.UTF8.GetString(buffer);
-var path = request.Split(' ')[1];
+var parsedRequest = new HttpRequest(request);
 
-var response = path == "/" ? HttpResponse.Ok() : HttpResponse.NotFound();
+var response = HttpResponse.Ok(parsedRequest);
 socket.Send(response);
 internal class HttpResponse
 {
-    internal static byte[] Ok() {
-        return Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+    internal static byte[] Ok(HttpRequest parsedRequest) {
+        return Encoding.UTF8.GetBytes(
+            "HTTP/1.1 200 OK\r\n\r\n" +
+            "Content-Type: text/plain\r\n\r\n" +
+            "Content-Length: " + $"{parsedRequest.PathContent.Length}" + "\r\n\r\n" +
+            "\r\n\r\n" +
+            $"{parsedRequest.PathContent}"
+            );
     }
 
     internal static byte[] NotFound() {
         return Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+}
+
+internal class HttpRequest
+{
+    internal HttpRequest(string request)
+    {
+        Request = request;
+        ParseRequest();
+    }
+
+    private string Request;
+    public string RequestType;
+    public string HttpVersion;
+    public string Path;
+    public string PathContent;
+
+    internal void ParseRequest()
+    {
+        RequestType = Request.Split(' ')[0];
+        Path = Request.Split(' ')[1];
+        HttpVersion = Request.Split(' ')[2];
+        PathContent = Path[(Path.IndexOf('/', Path.IndexOf('/') + 1) + 1)..];
+
     }
 }
