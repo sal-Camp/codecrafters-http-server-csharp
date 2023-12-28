9657d90e13a363db9f5d315fa053add38398c946
Salvatore Campisano <1342203+sal-Camp@users.noreply.github.com>
2023-12-27 20:26:39 -0500

    3.1

--- src/Server.cs
+++ src/Server.cs
@@ -8,11 +8,20 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-
 var buffer = new byte[1024];
-
 socket.Receive(buffer);
 
-var response = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+var request = Encoding.UTF8.GetString(buffer);
+var path = request.Split(' ')[1];
 
-socket.Send(response);
+var response = path == "/" ? HttpResponse.Ok() : HttpResponse.NotFound();
+internal class HttpResponse
+{
+    internal static byte[] Ok() {
+        return Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+    }
+
+    internal static byte[] NotFound() {
+        return Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+}
